8a068b5b7eb659d68b48cdb4406598b590387407
justcallmegoyo <justcallmegoyo@gmail.com>
2019-02-27 16:00:30 +0000

    Returns fizzbuzz if multiple of 3 and 5

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -20,4 +20,8 @@ describe 'fizzbuzz' do
   it 'returns the passed in number if not a multiple of 3 or 5' do
     expect(fizzbuzz(1)).to eq 1
   end
+
+  it "returns 'fizzbuzz' if number multiple of 3 and 5" do
+    expect(fizzbuzz(15)).to eq "fizzbuzz"
+  end
 end
