169afb21f485c49480da5a92fbdb167130ffdc27
justcallmegoyo <justcallmegoyo@gmail.com>
2019-02-27 15:52:05 +0000

    Multiple of 5 returns buzz

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -12,4 +12,8 @@ describe 'fizzbuzz' do
   it 'returns "fizz" when passed in 6' do
     expect(fizzbuzz(6)).to eq 'fizz'
   end
+
+  it 'returns "buzz" when passed in 10' do
+    expect(fizzbuzz(10)).to eq 'buzz'
+  end
 end
